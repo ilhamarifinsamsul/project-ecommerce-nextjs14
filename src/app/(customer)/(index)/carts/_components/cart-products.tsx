ac104cf15cbdd11c28658f78e336d4590c26b60b
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-10-10 10:00:30 +0700

    feat: add feature management cart product

---
+++ src/app/(customer)/(index)/carts/_components/cart-products.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import { rupiah } from "@/lib/utils";
+import React from "react";
+
+export default function CartProducts() {
+  const { products, increaseQuantity, decreaseQuantity, removeProduct } =
+    useCart();
+  return (
+    <>
+      {/* cart */}
+      <div
+        id="cart"
+        className="container max-w-[1130px] mx-auto flex flex-col gap-5 mt-[50px] text-gray-800"
+      >
+        {/* mapping */}
+        {products.map((cart) => (
+          <div
+            key={cart.id + cart.name}
+            className="product-total-card bg-white flex items-center justify-between p-5 rounded-[20px] border border-[#E5E5E5]"
+          >
+            <div className="flex items-center w-[340px] gap-5">
+              <div className="w-[120px] h-[70px] flex shrink-0 overflow-hidden items-center justify-center">
+                <img
+                  src={cart.image_url}
+                  className="w-full h-full object-contain"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <p className="font-semibold leading-[22px]">{cart.name}</p>
+                <p className="text-sm text-[#616369]">{cart.category_name}</p>
+              </div>
+            </div>
+            <div className="w-[150px] flex flex-col gap-1">
+              <p className="text-sm text-[#616369]">Price</p>
+              <p className="font-semibold text-[#0D5CD7] leading-[22px]">
+                {rupiah(Number(cart.price))}
+              </p>
+            </div>
+            <div className="w-[120px] flex flex-col gap-1">
+              <p className="text-sm text-[#616369]">Quantity</p>
+              <div className="flex items-center gap-3">
+                <button
+                  type="button"
+                  className="w-6 h-6 flex shrink-0"
+                  onClick={() => decreaseQuantity(cart.id)}
+                >
+                  <img src="assets/icons/minus-cirlce.svg" alt="minus" />
+                </button>
+                <p className="text-[#0D5CD7] font-semibold leading-[22px]">
+                  {cart.quantity}
+                </p>
+                <button
+                  type="button"
+                  className="w-6 h-6 flex shrink-0"
+                  onClick={() => increaseQuantity(cart.id)}
+                >
+                  <img src="assets/icons/add-circle.svg" alt="plus" />
+                </button>
+              </div>
+            </div>
+            <div className="w-[150px] flex flex-col gap-1">
+              <p className="text-sm text-[#616369]">Total</p>
+              <p className="font-semibold text-[#0D5CD7] leading-[22px]">
+                {rupiah(cart.price * cart.quantity)}
+              </p>
+            </div>
+            <button
+              type="button"
+              className="p-[12px_24px] bg-white rounded-full text-center font-semibold border border-[#E5E5E5]"
+              onClick={() => removeProduct(cart.id)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+      {/* end cart */}
+    </>
+  );
+}
